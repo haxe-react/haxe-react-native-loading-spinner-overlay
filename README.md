c8a34fc28e03cbf9f86ac9ac3a299ba9e542909a
Kevin Leung <kevinresol@gmail.com>
2017-01-05 11:54:56 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# haxe-react-native-loading-spinner-overlay
+
+Haxe Extern for react-native-loading-spinner-overlay
---
+++ src/react/native/loading_spinner_overlay/Spinner.hx
@@ -0,0 +1,4 @@
+package react.native.loading_spinner_overlay;
+
+@:jsRequire('react-native-loading-spinner-overlay', 'default')
+extern class Spinner extends react.ReactComponent {}
